c1ebd2b8c9fa3446e2ea3cabf0b14a8c946726bf
Daniel Penczak <penczakdaniel@gmail.com>
2024-08-14 21:00:07 -0500

    Start representation of commandTree to represent nested structure of possible commands

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Learning C and about database implementation. 
 
-Uses [AVL (Adelson-Velsky and Landis) trees](https://en.wikipedia.org/wiki/AVL_tree) which are self-balancing binary search trees to store key-value pairs. 
+# Build:
+repos\dpdb> gcc Source/sql/rdb.c -o dpdb.exe
--- Source/sql/rdb.c
+++ Source/sql/rdb.c
@@ -1,45 +1,46 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define NAME_SIZE 64
 
-struct column {
+typedef struct column {
     char name[NAME_SIZE];
     int type; // For simplicity, 0->int, 1->string
     int data_size;
     int index; // in table columns (not needed??)
-};
+} column;
 
-struct row {
+typedef struct row {
     char** values; // array of values, one for each column
-};
+} row;
 
-struct table {
+typedef struct table {
     char name[NAME_SIZE];
     int column_count;
     struct column* columns; // dynamically allocated array
     int row_count;
     struct row* rows; // dynamically allocated array
-};
+} table;
 
-struct database {
+typedef struct database {
     int table_count;
     struct table* tables; // dynamically allocated array
-};
+} database;
 
-void init_table(struct table* t, const char* name, int column_count) {
+void init_table(table* t, const char* name, int column_count) {
 
     strcpy(t->name, name);
     t->column_count = column_count;
-    t->columns = (struct column*)malloc(column_count * sizeof(struct column));
+    t->columns = (column*)malloc(column_count * sizeof(column));
     t->row_count = 0;
     t->rows = NULL; // Initially no rows
 }
 
 // Function to add a new row to a table
-void add_row(struct table* t, char** values) {
-    t->rows = (struct row*)realloc(t->rows, (t->row_count + 1) * sizeof(struct row));
+void add_row(table* t, char** values) {
+    t->rows = (row*)realloc(t->rows, (t->row_count + 1) * sizeof(row));
     t->rows[t->row_count].values = (char**)malloc(t->column_count * sizeof(char*));
     for (int i = 0; i < t->column_count; i++) {
         t->rows[t->row_count].values[i] = strdup(values[i]);
@@ -48,12 +49,12 @@ void add_row(struct table* t, char** values) {
 }
 
 // Function to create a new database
-void init_database(struct database* db, int table_count) {
+void init_database(database* db, int table_count) {
     db->table_count = table_count;
-    db->tables = (struct table*)malloc(table_count * sizeof(struct table));
+    db->tables = (table*)malloc(table_count * sizeof(table));
 }
 
-void free_table(struct table* t) {
+void free_table(table* t) {
     for (int i = 0; i < t->row_count; i++)
     {
         for (int j = 0; j < t->column_count; j++)
@@ -67,7 +68,7 @@ void free_table(struct table* t) {
 }
 
 void test() {
-    struct database db;
+    database db;
     init_database(&db, 2); // Initialize a database with space for 2 tables
 
     init_table(&db.tables[0], "Table1", 3); // Initialize first table with 3 columns
@@ -85,7 +86,7 @@ void test() {
     add_row(&db.tables[0], row1_values);
 
     // Print the first row of the first table
-    printf("First row: ID = %s, Name = %s, Age = %s\n",
+    printf("\n\nFirst row: ID = %s, Name = %s, Age = %s\n",
         db.tables[0].rows[0].values[0],
         db.tables[0].rows[0].values[1],
         db.tables[0].rows[0].values[2]);
@@ -95,16 +96,102 @@ void test() {
     free(db.tables);
 }
 
+typedef struct commandTree {
+    char command[99]; // "table"
+    bool isOptional; // true
+    struct commandTree* children[99]; // "new", "update", "drop" trees
+    char parameters[99];
+    bool isLeaf;
+    int (*func)(char*);
+} commandTree;
+
+int fun(char* input) { printf("Value of a is %s\n", input); return 1; }
+
+typedef struct word {
+    int length;
+    char text[99];
+} word;
+
+word getWord(char* input) {
+    word out = {};
+    out.length = 0;
+    //out.text = ;
+    for (int i = 0; i < 99; i++)
+    {
+        if (input[i] == ' ') {
+            return out;
+        }
+        out.length = i + 1;
+        out.text[i] = input[i];
+    }
+    return out;
+}
+
+commandTree getCommandTree() {
+    commandTree tr = {};
+    //tr.command = NULL;
+    tr.isOptional = false;
+    tr.isLeaf = false;
+    tr.func = &fun;
+    //tr.children = 
+    return tr;
+}
+
+int getCommand(char* input) {
+    commandTree curr = getCommandTree();
+    int inputPtr = 0;
+    for (int i = 0; i < 99; i++)
+    {
+
+    }
+    return 1;
+}
 
 int main() {
-    /*struct database* db = (struct database*)malloc(sizeof(struct database));
-    if (db == NULL) {
-        perror("malloc");
-        return EXIT_FAILURE;
+    char input[100];
+    printf("Enter command:\n");
+    scanf(" %s", &input);
+
+    //if (command == "") {
+    //
+    //}
+    //commandTree cmd = getCommandTree();
+
+    //int x = (cmd.func)(command);
+    //printf("\nx is %d", x);
+
+    char *input = "hello world";
+    char commands[99][99] = {{ 0 }};
+    int commandCt = 1, i = 0, j = 0;
+    while (input[i] != '\0')
+    {
+        if (input[i] == ' ') {
+            if (j != 0) {
+                j = 0;
+                commandCt++;
+            }
+            i++;
+            continue;
+        }
+        commands[commandCt - 1][j] = input[i];
+        i++;
+        j++;
     }
 
-    create_table(db);
+    printf("\ncount:|%d|", commandCt);
+    for (int i = 0; i < commandCt; i++)
+    {
+        printf("\n|%s|", commands[i]);
+    }
 
-    return 0;*/
-    test();
+    /*int pt = 0;
+    printf("\nwhole text is |%s|", commands[0]);
+    word w = getWord(commands[0]);
+    printf("\nw.length is |%d|", w.length);
+    printf("\nw.text is |%s|", w.text);
+
+    printf("\ntext after point is |%s|", commands[1]);*/
+
+    //test();
+    return 0;
 }
